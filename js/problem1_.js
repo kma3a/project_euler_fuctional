3a998c775143b93081dd393ff4647c4b67f164b9
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-06-05 10:06:45 -0400

    adding the underscore version of problem 1

--- js/problem1_.js
+++ js/problem1_.js
@@ -1,2 +1,28 @@
 var _ = require('underscore');
 
+var isDiv = function(num) {
+  return num % 3 === 0 || num % 5 === 0;
+}
+
+var addIndexes = function(x, i) {
+  return i + 1;
+}
+
+var array = function(upperLimit) {
+  return Array.apply(null, new Array(upperLimit)).map(addIndexes);
+};
+
+var filterArray = function(limit) {
+  return _.filter(array(limit), isDiv);
+};
+
+var calcSum = function(limit) {
+  return _.reduce(filterArray(limit -1), sum);
+}
+
+var sum = function(previous, current) {
+  return previous + current;
+}
+
+
+console.log(calcSum(1000));
