add53b4b352b02e1b56ab577f95060ef027a4270
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-06-12 15:05:15 -0400

    adding the how to do problem 3 in javascript

---
+++ js/problem3.js
@@ -0,0 +1,25 @@
+function isPrime(num) {
+  for(var start = Math.round(Math.sqrt(num)); start>1; start--)  {
+    if( num % start == 0 ) {
+      return false
+    }
+  }
+  return true
+}
+
+function getFactors(num) {
+  var factorArray = [];
+  for(var start = Math.round(Math.sqrt(num)); start>1; start--)  {
+    if( num % start == 0 ) {
+      factorArray.push(start, num/start)
+    }
+  }
+  return factorArray;
+}
+
+function getPrimeFactors(num) {
+  return getFactors(num).filter(isPrime)
+}
+
+console.log(getPrimeFactors(13195))
+console.log(getPrimeFactors(600851475143))
