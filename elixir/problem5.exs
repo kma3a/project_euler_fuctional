d12fb06e3703def57b67f33dc79067f303e05ff7
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-06-26 14:54:33 -0400

    adding the recursion version of problem 5 that goes faster

--- elixir/problem5.exs
+++ elixir/problem5.exs
@@ -33,5 +33,47 @@ end
 
 SmallDiv.getDiv(5)
 SmallDiv.getDiv(10)
-SmallDiv.getDiv(20)
+
+
+defmodule SmallDivTwo do
+
+  def getDiv(upLimit) do
+    findDiv(upLimit * upLimit, upLimit)
+  end
+
+  defp findDiv(startNum, upLim) when startNum >= 100000000000 do
+    IO.puts upLim
+  end
+
+  defp findDiv(startNum, upLim) do
+    if checkDiv(startNum, upLim) do
+      startNum
+    else
+      findDiv(startNum + upLim, upLim)
+    end
+  end
+
+  defp checkDiv(num, upLim) when upLim == 1 do
+    IO.puts num
+    true
+  end
+
+  defp checkDiv(num, upLim) do
+    if isDiv(num, upLim) do
+      checkDiv(num, upLim - 1)
+    else
+      false
+    end
+  end
+
+  defp isDiv(num, divVar) do
+    rem(num, divVar) == 0
+  end
+
+end
+
+SmallDivTwo.getDiv(5)
+SmallDivTwo.getDiv(10)
+SmallDivTwo.getDiv(20)
+
 
