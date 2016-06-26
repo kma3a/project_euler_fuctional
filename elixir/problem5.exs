7c908d5565db470655b5fccd6cc90ada0aea2cde
Kelsey Ando <kelsey.ando@detroitlabs.com>
2016-06-26 14:46:29 -0400

    adding the rest of the code for elixir

--- elixir/problem5.exs
+++ elixir/problem5.exs
@@ -1,19 +1,37 @@
 defmodule SmallDiv do
 
-  def underN(num, upLim) do
+  def getDiv(upLimit) do
+    findDiv(upLimit * upLimit, upLimit)
+  end
+
+  defp findDiv(startNum, upLim) when startNum >= 100000000000 do
+    IO.puts upLim
+  end
+
+  defp findDiv(startNum, upLim) do
+    if checkDiv(startNum, upLim) do
+      IO.puts startNum
+    else
+      findDiv(startNum + upLim, upLim)
+    end
+  end
+
+  defp underN(num, upLim) do
     for n <- 1..upLim, do: isDiv(num, n)
   end
 
-  def checkDiv(num, upLim) do
-    IO.puts Enum.all?(underN(num, upLim), fn(n) -> n == 0 end)
+  defp checkDiv(num, upLim) do
+    Enum.all?(underN(num, upLim), fn(n) -> n == 0 end)
   end
     
 
-  def isDiv(num, divVar) do
+  defp isDiv(num, divVar) do
     rem(num, divVar)
   end
 
 end
 
-SmallDiv.checkDiv(2520, 10)
+SmallDiv.getDiv(5)
+SmallDiv.getDiv(10)
+SmallDiv.getDiv(20)
 
